037011a98aaa05d49334901a7b1112aba28b13f9
heidi3115 <heidi3115@gmail.com>
2025-11-06 01:02:37 +0900

    feat: CoinListRow 컴포넌트 분리

---
+++ app/components/ui/CoinListRow.tsx
@@ -0,0 +1,51 @@
+import {FaStar} from "react-icons/fa";
+import {CiStar} from "react-icons/ci";
+import Image from "next/image";
+import {formatNumber} from "@/utils/commonUtils";
+import {Coin} from "@/app/types/coin";
+
+type CoinListRowProps = {
+    coin: Coin;
+    onToggleFavorite: (coinId: string) => void;
+    isFavorited: (coinId: string) => boolean;
+}
+
+export const CoinListRow = ({
+                                coin,
+                                onToggleFavorite,
+                                isFavorited,
+}:CoinListRowProps) => {
+    return (
+        <tr>
+            <td className="flex items-center gap-2">
+                <button onClick={() => onToggleFavorite(coin.id)}>
+                    {isFavorited(coin.id) ? (
+                        <FaStar className="text-yellow-400" />
+                    ) : (
+                        <CiStar />
+                    )}
+                </button>
+                <Image
+                    src={coin.icon}
+                    alt={coin.name}
+                    width={24}
+                    height={24}
+                />
+                <span className="font-semibold">{coin.symbol}</span>
+                <span className="text-gray-400">{coin.name}</span>
+            </td>
+            <td>
+                <div className="flex flex-col gap-1">
+                    <span>{coin.price}</span>
+                    <span>{formatNumber(coin.price)}</span>
+                </div>
+            </td>
+            <td className={`text-center ${coin.change24h >= 0 ? 'text-green-500' : 'text-red-500'}`}>
+                {coin.change24h > 0 ? "+" : ""}{coin.change24h}%
+            </td>
+            <td className="text-right">{formatNumber(coin.volume24h)}</td>
+            <td className="text-right">{formatNumber(coin.marketCap)}</td>
+        </tr>
+
+    )
+}
--- app/components/ui/CoinListTable.tsx
+++ app/components/ui/CoinListTable.tsx
@@ -9,11 +9,8 @@ import {
     useReactTable
 } from "@tanstack/react-table";
 import { Coin } from "@/app/types/coin";
-import { FaStar } from "react-icons/fa";
-import { CiStar } from "react-icons/ci";
-import Image from "next/image";
-import { formatNumber } from "@/utils/commonUtils";
 import { SortableHeader } from "@/app/components/ui/SortableHeader";
+import {CoinListRow} from "@/app/components/ui/CoinListRow";
 
 type TableProps = {
     coins: Coin[]
@@ -71,37 +68,13 @@ export const CoinListTable = ({coins, onToggleFavorite, isFavorited}: TableProps
              ))}
              </thead>
              <tbody>
-             {table.getRowModel().rows.map((row) => (
-                 <tr key={row.id}>
-                     <td className="flex items-center gap-2">
-                            <button onClick={() => onToggleFavorite(row.original.id)}>
-                                {isFavorited(row.original.id) ? (
-                                    <FaStar className="text-yellow-400" />
-                                ) : (
-                                    <CiStar />
-                                )}
-                            </button>
-                         <Image
-                             src={row.original.icon}
-                             alt={row.original.name}
-                             width={24}
-                             height={24}
-                         />
-                         <span className="font-semibold">{row.original.symbol}</span>
-                         <span className="text-gray-400">{row.original.name}</span>
-                     </td>
-                     <td>
-                         <div className="flex flex-col gap-1">
-                             <span>{row.original.price}</span>
-                             <span>{formatNumber(row.original.price)}</span>
-                         </div>
-                     </td>
-                     <td className={`text-center ${row.original.change24h >= 0 ? 'text-green-500' : 'text-red-500'}`}>
-                         {row.original.change24h > 0 ? "+" : ""}{row.original.change24h}%
-                     </td>
-                     <td className="text-right">{formatNumber(row.original.volume24h)}</td>
-                     <td className="text-right">{formatNumber(row.original.marketCap)}</td>
-                 </tr>
+             {table.getRowModel().rows.map((it) => (
+                 <CoinListRow
+                    key={it.id}
+                    coin={it.original}
+                    onToggleFavorite={onToggleFavorite}
+                    isFavorited={isFavorited}
+                 />
              ))}
              </tbody>
          </table>
